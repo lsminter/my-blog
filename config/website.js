0caabcd210299209c1ae0f1edf1eabac73f25b1d
lucas <lsminteris@gmail.com>
2019-12-06 09:57:53 -0800

    my first push

---
+++ config/website.js
@@ -0,0 +1,29 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
+  siteTitle: 'Lucas Minter', // Navigation and Site Title
+  siteTitleAlt: 'Lucas Minter', // Alternative Site title for SEO
+  siteTitleShort: 'Lucas Minter', // short_name for manifest
+  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription: 'I am a Learner Advocate at egghead.io!',
+  author: 'Lucas Minter', // Author for schemaORGJSONLD
+  organization: '',
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  // userTwitter: '@eggheadio', // Twitter Username
+  ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
+  ogLanguage: 'en_US',
+  googleAnalyticsID: '',
+
+  // Manifest and Progress color
+  themeColor: '#0000ff',
+  backgroundColor: '#2b2e3c',
+
+  // Social component
+  twitterUrl: '',
+  twitterHandle: '',
+  githubUrl: 'https://github.com/lsminter/',
+  githubHandle: 'lsminter',
+  fbAppID: '',
+}
