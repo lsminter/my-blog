43c079670a0f2039c0aca25d4781d5426fdc8e48
Lucas Minter <lucasminter@Lucass-MacBook-Pro.local>
2020-02-20 12:23:02 -0800

    added styles.css file, added picture of me, edited about file to only include necessary code

--- src/components/bio/index.js
+++ src/components/bio/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import dogo from './dogo.jpg'
+import me from './me.png';
+import './styles.css';
 
 
 export default function Bio () {
   return ( 
     <div>
-      <img src={dogo} />
+      <img src={me} class="me" />
       <p>
       Our synergy development lifecycle enables senior, corporate milestones. We aim to virtually revolutionize our low hanging fruit by iteratively virtualising our company-wide customer-focused capabilities. Going forward, our best-in-class proposition will deliver value to agile workflows. End-to-end best practices globally enable value-added drivers for our stakeholders.
       </p>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,12 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { css } from '@emotion/core'
-import styled from '@emotion/styled'
 import Layout from '@eggheadio/gatsby-theme-egghead-blog/src/components/Layout'
-import Link from '@eggheadio/gatsby-theme-egghead-blog/src/components/Link'
-import { useTheme } from '@eggheadio/gatsby-theme-egghead-blog/src/components/Theming'
 import Container from '@eggheadio/gatsby-theme-egghead-blog/src/components/Container'
-import { rhythm } from '@eggheadio/gatsby-theme-egghead-blog/src/lib/typography'
 import Bio from '../components/bio'
 
-const Description = styled.p`
-  margin-bottom: 10px;
-  display: inline-block;
-`
+
 export default function About({ data: { site, allMdx } }) {
-  const theme = useTheme()
   return (
     <Layout site={site}>
       <Container
