4f575f96e4ff3d34670aa23b1eec4a266eee99b6
lsminter <lsminteris@gmail.com>
2021-03-11 15:18:26 -0800

    changed website title

--- config/website.js
+++ config/website.js
@@ -6,7 +6,7 @@ module.exports = {
   siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'I am a Learner Advocate at egghead.io!',
+  siteDescription: 'I am a Production Engineer at egghead.io!',
   author: 'Lucas Minter', // Author for schemaORGJSONLD
   organization: '',
 
